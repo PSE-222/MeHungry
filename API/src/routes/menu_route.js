9cb58bdf3061996f248d83d134a7ba7a14ddfd80
canceller026 <dat.phanpan7@hcmut.edu.vn>
2023-03-30 15:52:13 +0700

    update http method for menu

--- API/src/routes/menu_route.js
+++ API/src/routes/menu_route.js
@@ -9,7 +9,7 @@ router.post("/add-item", menu_controller.add_item);
 
 router.post("/update-item", menu_controller.update_item);
 
-router.get("/delete-item/:id", menu_controller.delete_item);
+router.post("/delete-item/:id", menu_controller.delete_item);
 
 router.get("/menu", menu_controller.view_menu);
 
