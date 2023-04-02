0ef6b79d291dbe7baea710b2c2c1e981bd34dd55
Dat Phan Minh <darkstardragon9@gmail.com>
2023-04-02 23:28:04 +0700

    add cors

--- API/app.js
+++ API/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const body_parser = require("body-parser");
-
+const cors = require("cors");
 require("dotenv").config()
-// const cors = require("cors");
+
 const table_router = require("./src/routes/table_route");
 const order_router = require("./src/routes/order_route");
 const menu_router = require("./src/routes/menu_route");
@@ -13,7 +13,7 @@ const admin_router = require("./src/routes/admin_route");
 app.use(express.json());
 app.use(body_parser.json());
 
-// app.use(cors());
+app.use(cors());
 app.use("/api", table_router);
 app.use("/api", order_router);
 app.use("/api", menu_router);
