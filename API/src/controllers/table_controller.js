24479cf61fdab111595278ed3eff9a52ef08cd2a
Dat Phan Minh <darkstardragon9@gmail.com>
2023-04-04 11:40:03 +0700

    update table api

--- API/src/controllers/table_controller.js
+++ API/src/controllers/table_controller.js
@@ -3,13 +3,14 @@
 // [ ] is_occupied -> status: 0-free,1-requested,2-serving,3-checkout
 // [ ] What if name inputted is empty?
 
+
 const db_object = require('../db/config');
 const { create_order, count_order } = require('../controllers/order_controller')
 const { create_payment } = require('../controllers/payment_controller')
 const table_collection = db_object.getDb().collection("Table")
 
 const status_arr =["free","requested","serving","checkout"]
-
+var pending_list = Array(16).fill("");
 async function create_order_and_payment (table_number){
 	const new_order_id = (await count_order() + 1).toString();
 	await create_order(new_order_id, table_number);
@@ -19,38 +20,34 @@ exports.request_table = async (req,res) =>{
 	const table_id = req.params.id;
 	const table_info = await table_collection.findOne({table_id: table_id,});
 	
-	if (!table_info){
-		res.status(404).send({msg: "Invalid Table Number"});
-		return;
-	}
-	if ( table_info["status"] != 0){
-		res.send({msg :"Table Is Not Available!!"});
-		return;
-	}
+	if ( !table_info ){
+		return res.status(404).send({msg: "Invalid Table Number"});
 
-	await table_collection.updateOne({table_id: table_id}, {$set: {status: 1}})
-	const new_table_info = await table_collection.findOne({table_id: table_id},);
+	}
+	if ( table_info["status"] != 0 )
+		return res.send({msg :"Table Is Not Available!!"});
 
-	res.send(new_table_info);
+	// await table_collection.updateOne({table_id: table_id}, {$set: {status: 1}})
+	// const new_table_info = await table_collection.findOne({table_id: table_id},);
+
+	res.send(table_info);
 };
+
 exports.assign_table = async (req,res) =>{
 	const table_number = req.params.number;
 
 	const table_info = await table_collection.findOne({table_number: table_number,});
-	if (!table_info){
-		res.status(404).send({msg: "Invalid Table Number"});
-		return;
-	}
-	if (table_info["status"] != 2){
-		res.send({msg :"Invalid Table Assignment!!!"});
-		return;
-	}
+	if ( !table_info )
+		return res.status(404).send({msg: "Invalid Table Number"});
+		
+	if (table_info["status"] != 0)
+		return res.send({msg :"Invalid Table Assignment!!!"});
 
 	if (table_info["customer_name"] != "" ){
 		res.send({msg: "Table Is Being Occupied!!!"});
 		return;
 	}
-	await table_collection.updateOne({table_number: table_number}, {$set: {customer_name: req.params.name}})
+	await table_collection.updateOne({table_number: table_number}, {$set: {status: 1, customer_name: req.params.name}})
 	await create_order_and_payment(table_info["table_number"]);
 	const new_table_info = await table_collection.findOne({table_number: table_number},);
 
