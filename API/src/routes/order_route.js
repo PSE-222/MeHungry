0c95859cf959af3cc4ca4c48c4869e43e2e32b1c
Dat Phan Minh <darkstardragon9@gmail.com>
2023-04-04 23:44:21 +0700

    fix order route

--- API/src/routes/order_route.js
+++ API/src/routes/order_route.js
@@ -10,7 +10,7 @@ router.get("/view-current-orders", auth, order_controller.view_current_orders);
 
 router.get("/orders", auth, order_controller.view_orders)
 
-router.post("/add-items/:id", order_controller.add_item_to_order);
+router.post("/add-items/:number", order_controller.add_item_to_order);
 
 router.post("/finish-order/:id", auth, order_controller.finish_order);
 
